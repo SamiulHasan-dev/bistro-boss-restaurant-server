43b46161f9a27d8aedffb255f0b21d0607dcade7
SamiulHasan-dev <samiul.199897@gmail.com>
2024-05-29 23:54:43 +0600

    create post for cart

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
 
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartCollection = client.db("bistroDb").collection("carts");
 
     app.get('/menu', async(req, res)=>{
         const result = await menuCollection.find().toArray();
@@ -37,6 +38,14 @@ async function run() {
     app.get('/reviews', async(req, res)=>{
         const result = await reviewCollection.find().toArray();
         res.send(result);
+    })
+
+
+    //carts collection
+    app.post('/carts', async(req, res)=>{
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
@@ -57,3 +66,16 @@ app.get('/', (req, res)=>{
 app.listen(port, ()=>{
     console.log(`Bistro Boss is running on ${port}`);
 })
+
+
+/* 
+  ----------------------
+  Naming Convention
+  ---------------------
+  app.get('/users')
+  app.get('/users/:id')
+  app.post('/users')
+  app.put('/users/:id')
+  app.patch('/users/:id')
+  app.delete('/users/:id')
+*/
