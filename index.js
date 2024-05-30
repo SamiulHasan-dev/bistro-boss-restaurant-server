45e18657f512227f52976f9c7a8bd1073883f58a
SamiulHasan-dev <samiul.199897@gmail.com>
2024-05-31 03:05:48 +0600

    delete cart by id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -52,6 +52,13 @@ async function run() {
       const email = req.query.email;
       const query = {email: email}
       const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.delete('/cart/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 
